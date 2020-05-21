ff8da9cef9e522874075d7b08cd9c1c91ceb8740
ddudarin7 <nsdudo@yahoo.com>
2020-05-21 19:50:09 +0200

    nav bar and components added

--- vet-diagnostic-system-angular/src/app/vet/vet.module.ts
+++ vet-diagnostic-system-angular/src/app/vet/vet.module.ts
@@ -2,14 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page/home-page.component';
 import { RouterModule, Routes } from '@angular/router';
+import { AddPatientFormComponent } from './add-patient-form/add-patient-form.component';
+import { ShowAllPatientsComponent } from './show-all-patients/show-all-patients.component';
+import { SearchComponent } from './search/search.component';
 
 
 const routes: Routes = [
-  {path:'home',component: HomePageComponent}];
+  {path:'home',component: HomePageComponent,children:[
+    {path:'',component:ShowAllPatientsComponent},
+    {path:'add-patient',component:AddPatientFormComponent},
+    {path:'search',component:SearchComponent}
+  ]}];
 
 
 @NgModule({
-  declarations: [HomePageComponent],
+  declarations: [
+    HomePageComponent, 
+    AddPatientFormComponent, 
+    ShowAllPatientsComponent, SearchComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes)
