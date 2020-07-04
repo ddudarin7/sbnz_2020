a1390b210f81f6c04f8d22ce87077d99ac197fd0
ddudarin7 <nsdudo@yahoo.com>
2020-07-04 23:41:31 +0200

    No diagnose+test

--- vet-diagnostic-system-angular/src/app/core/services/diagnose.service.ts
+++ vet-diagnostic-system-angular/src/app/core/services/diagnose.service.ts
@@ -3,6 +3,7 @@ import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import {Symptom} from '../../shared/model/symptom';
 import {Diagnose} from '../../shared/model/diagnose';
 import {BehaviorSubject} from 'rxjs';
+import { DiagnoseResult } from 'src/app/shared/model/diagnose-result';
 
 @Injectable({
   providedIn: 'root'
@@ -22,9 +23,9 @@ export class DiagnoseService {
     this.source.next(patientRecordNumber);
   }
 
-  public diagnose(patientRecordNumber: string, symptoms: Symptom[]): Promise<Diagnose> {
-    return this.http.post<Diagnose>('http://localhost:8081/api/diagnose/' + patientRecordNumber, symptoms,
-      {headers: this.headers, withCredentials: true}).toPromise().then(res => res as Diagnose);
+  public diagnose(patientRecordNumber: string, symptoms: Symptom[]): Promise<Diagnose[]> {
+    return this.http.post<Diagnose[]>('http://localhost:8081/api/diagnose/' + patientRecordNumber, symptoms,
+      {headers: this.headers, withCredentials: true}).toPromise().then(res => res as Diagnose[]);
   }
 
   public confirmDiagnose(diagnose: Diagnose): Promise<Diagnose> {
